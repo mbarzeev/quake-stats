2ba384bc9c6b091a2843d3ea4abb9b7f0558f18e
Matti Bar-Zeev <mstti.bar-zeev@hp.com>
2013-09-04 14:18:51 +0300

    Add the real names of the maps

--- app/scripts/services/gameslog-srv.js
+++ app/scripts/services/gameslog-srv.js
@@ -13,8 +13,14 @@ angular.module('quakeStatsApp')
 		"q3ctf3" : "The Stronghold",
  		"q3ctf4" : "Space CTF",
  		"speedyctf" : "Speedy",
- 		"ump3ctf4" : "Snow",
- 		"q3ctf2": "Troubled Waters"
+ 		"q3ctf2": "Troubled Waters",
+        "ump3ctf1": "Breakdown",
+        "ump3ctf2": "Hydrophobia",
+        "ump3ctf3": "Got Wood",
+        "ump3ctf4": "Frozen Colors",
+        "ump3ctf5": "Railfest 2: 7Zark7 Island",
+        "ump3ctf6": "Like spinning plates",
+        "ump3ctf7": "The Strong Jaeger"
  	};
  	this.gameslog = null;
  	this.loglines = null;
