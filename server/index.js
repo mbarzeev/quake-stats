44da335a5ef7f1d3143a3a17f7504e75e32015be
Matti Bar-Zeev <mstti.bar-zeev@hp.com>
2013-10-02 15:49:17 +0300

    Ongoing: Enable error codes from Express server

--- server/index.js
+++ server/index.js
@@ -5,12 +5,26 @@ var app = express();
 app.configure(function () {
     app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
     app.use(express.bodyParser());
+    app.use(function(err, req, res, next){
+    	console.log(err);
+		res.send(500, err);
+	});
 });
 
 app.get('/rest/games/log', function(req, res){
 	fs.readFile('./games.log', 'utf8', function (err,data) {
 	  if (err) {
 	    return console.log(err);
+	  }
+	  res.send(data);
+	});
+  
+});
+
+app.get('/rest/games/qconsole', function(req, res, next){
+	fs.readFile('./qconsole.log', 'utf8', function (err,data) {
+	  if (err) {
+	     next(new Error('Something went wrong :-( '));
 	  }
 	  res.send(data);
 	});
