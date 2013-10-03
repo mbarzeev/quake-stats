230771a7bca3b0c449b59944e4dba3aec908b55e
Matti Bar-Zeev <mstti.bar-zeev@hp.com>
2013-10-03 16:49:15 +0300

    Ongoing: Add menu items to the navigation bar

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,23 @@
 'use strict';
 
 angular.module('quakeStatsApp')
-    .controller('MainCtrl', [
-        function () {
+    .controller('MainCtrl', ['$scope',
+        function ($scope) {
+            var menu = {};
+            menu.items = [
+                {label:'Dashboard', data:'dashboard'},
+                {label:'Flags', data:'flags'},
+                {label:'Kills', data:'kills'},
+                {label:'Players', data:'players'}
+            ];
+            $scope.menu = menu;
+            $scope.menu.selectedMenuItemIndex = -1;
+
+            $scope.onMenuItemClick = function(index) {
+                if ($scope.menu.selectedMenuItemIndex !== index) {
+                    $scope.menu.selectedMenuItemIndex = index;
+                }
+            };
         }
     ]
 );
