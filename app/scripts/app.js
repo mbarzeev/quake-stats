8a6e33f1ddf4fb73fb070a7e7fd73d2e9d17d7fe
Matti Bar-Zeev <mstti.bar-zeev@hp.com>
2013-10-09 00:37:29 +0300

    Ongoing: Add the Kills view

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -51,6 +51,28 @@ angular.module('quakeStatsApp', ['ngResource'])
                     }]
                 }
             })
+            .when('/kills', {
+                templateUrl: 'views/kills.html',
+                controller: 'KillsCtrl',
+                resolve: {
+                    gamesLog: ['$q', 'GamesLogService', function($q, GamesLogService) {
+                        var dfd = $q.defer();
+                        GamesLogService.loadLog().then(
+                            function(result) {
+                                dfd.resolve({
+                                    success: true,
+                                    result : result
+                                });
+                            }, function(error) {
+                                dfd.resolve({
+                                    success : false,
+                                    reason : error
+                                });
+                            });
+                        return dfd.promise;
+                    }]
+                }
+            })
             .otherwise({
                 redirectTo: '/flags'
             });
---
+++ app/scripts/controllers/kills-ctrl.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('quakeStatsApp')
+    .controller('KillsCtrl', ['$scope',
+        function ($scope) {
+        
+    }]);
