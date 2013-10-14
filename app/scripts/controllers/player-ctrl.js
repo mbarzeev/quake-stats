4993f80c899c48c2646ceb03704e2e73c85c0915
Matti Bar-Zeev <mstti.bar-zeev@hp.com>
2013-10-14 13:26:10 +0300

    Add the Player view

---
+++ app/scripts/controllers/player-ctrl.js
@@ -0,0 +1,14 @@
+'use strict';
+
+angular.module('quakeStatsApp')
+    .controller('PlayerCtrl', ['$scope', 'gamesLog', 'KillsService',
+        function ($scope, gamesLog, KillsService) {
+        
+        $scope.killStats = {};
+
+        if (gamesLog.success === false) {
+            console.log('Cannot load games.log - you wil not be able to see kills stats');
+        } else {
+            $scope.killsStats = KillsService.getKillsStats(gamesLog.result);
+        }
+	}]);
