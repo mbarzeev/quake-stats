f1bf82ae11c4bf3ff4e173e6231915a4d383a065
Matti Bar-Zeev <mstti.bar-zeev@hp.com>
2013-11-22 16:43:13 +0200

    Ongoing: Create a drop down for easy navigation to each player

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,23 +1,5 @@
 'use strict';
 
 angular.module('quakeStatsApp')
-    .controller('MainCtrl', ['$scope',
-        function ($scope) {
-            var menu = {};
-            menu.items = [
-                {label:'Dashboard', data:'dashboard'},
-                {label:'Flags', data:'flags'},
-                {label:'Kills', data:'kills'},
-                {label:'Players', data:'players'}
-            ];
-            $scope.menu = menu;
-            $scope.menu.selectedMenuItemIndex = -1;
-
-            $scope.onMenuItemClick = function(index) {
-                if ($scope.menu.selectedMenuItemIndex !== index) {
-                    $scope.menu.selectedMenuItemIndex = index;
-                }
-            };
-        }
-    ]
-);
+    .controller('MainCtrl', function () {
+});
---
+++ app/scripts/controllers/navigation-ctrl.js
@@ -0,0 +1,35 @@
+'use strict';
+
+angular.module('quakeStatsApp')
+	.controller('NavigationCtrl', ['$scope', 'GamesLogService', 'KillsService',
+		function ($scope, GamesLogService, KillsService) {
+			var me = this;
+			GamesLogService.loadLog().then(
+				function(result) {
+					me.onGamesLogLoaded(result);
+				},
+				function(error) {
+					console.error(error);
+					return;
+				});
+
+			this.onGamesLogLoaded = function(log) {
+				$scope.killsStats = KillsService.getKillsStats(log);
+				$scope.players = $scope.killsStats.players;
+			};
+
+			var menu = {};
+            menu.items = [
+                {label:'Dashboard', data:'dashboard'},
+                {label:'Flags', data:'flags'},
+                {label:'Kills', data:'kills'}
+            ];
+            $scope.menu = menu;
+            $scope.menu.selectedMenuItemIndex = -1;
+
+            $scope.onMenuItemClick = function(index) {
+                if ($scope.menu.selectedMenuItemIndex !== index) {
+                    $scope.menu.selectedMenuItemIndex = index;
+                }
+            };
+		}]);
