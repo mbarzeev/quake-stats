4ab02593673acc6914f3c23acee441f32baeb7c0
Matti Bar-Zeev <mstti.bar-zeev@hp.com>
2013-11-24 10:28:19 +0200

    Fix: use the logStartIndex to get the map timeline

--- app/scripts/filters/map-name-fltr.js
+++ app/scripts/filters/map-name-fltr.js
@@ -1,12 +1,14 @@
 'use strict';
 
 angular.module('quakeStatsApp').
-    filter('MapNameFilter', ['MapConstants', function (MapConstants) {
-        return function (input) {
-			var key = input.toLowerCase();
-			if (MapConstants.hasOwnProperty(key)) {
-                return MapConstants[key];
-            }
-            return 'N/A';
-        };
-    }]);
+	filter('MapNameFilter', ['MapConstants', function (MapConstants) {
+		return function (input) {
+			if (input) {
+				var key = input.toLowerCase();
+				if (MapConstants.hasOwnProperty(key)) {
+					return MapConstants[key];
+				}
+			}
+			return 'N/A';
+		};
+	}]);
